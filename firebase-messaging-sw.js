b0458b33dfd5966570b254026052c05f4712ed19
zxcvbn848 <zxcvbn884488@gmail.com>
2021-11-11 15:02:01 +0800

    add onMessage

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,15 +2,15 @@ import { getMessaging } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-
 import { onBackgroundMessage } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-messaging-sw.js";
 
 const messaging = getMessaging();
-onBackgroundMessage(messaging, (payload) => {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  // Customize notification here
-  const notificationTitle = 'Background Message Title';
-  const notificationOptions = {
-    body: 'Background Message body.',
-    icon: '/firebase-logo.png'
-  };
+// onBackgroundMessage(messaging, (payload) => {
+//   console.log('[firebase-messaging-sw.js] Received background message ', payload);
+//   // Customize notification here
+//   const notificationTitle = 'Background Message Title';
+//   const notificationOptions = {
+//     body: 'Background Message body.',
+//     icon: '/firebase-logo.png'
+//   };
 
-  self.registration.showNotification(notificationTitle,
-    notificationOptions);
-});
+//   self.registration.showNotification(notificationTitle,
+//     notificationOptions);
+// });
